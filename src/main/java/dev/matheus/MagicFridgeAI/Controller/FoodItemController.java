9b334df9040b351b4d66a84d35a734bcb8d0b03b
Matheus Mello <matheusgmello1@gmail.com>
2025-08-18 21:26:41 -0300

    feat: adiciona swagger para documentacao das rotas de FoodItem e RecipeController

--- src/main/java/dev/matheus/MagicFridgeAI/Controller/FoodItemController.java
+++ src/main/java/dev/matheus/MagicFridgeAI/Controller/FoodItemController.java
@@ -3,6 +3,12 @@ package dev.matheus.MagicFridgeAI.Controller;
 import dev.matheus.MagicFridgeAI.DTOs.FoodItemDTO;
 import dev.matheus.MagicFridgeAI.Mapper.FoodItemMapper;
 import dev.matheus.MagicFridgeAI.Service.FoodItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +18,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/food")
+@Tag(name = "Food", description = "Endpoints para gerenciamento de itens na geladeira.")
 public class FoodItemController {
 
     private final FoodItemService foodItemService;
@@ -24,13 +31,30 @@ public class FoodItemController {
 
     // POST REQUEST - Cria um item
     @PostMapping("/criar")
+    @Operation(summary = "Cadastro de um item para geladeira",
+            description = "Cria um novo item para a geladeira com os detalhes fornecidos. categoria deve ser uma das predefinidas," +
+                    "A categoria do item de comida. Deve ser um dos seguintes valores: " +
+                    "LATICINIOS, CARNES, VEGETAIS, FRUTAS, GRAOS, FRUTOS_DO_MAR, BEBIDAS, " +
+                    "LANCHES, CONDIMENTOS, CONGELADOS, PADARIA, ENLATADOS, ESPECIARIAS, OLEOS, DOCES ou OUTROS.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Item criado com sucesso.",
+                    content = @Content(schema = @Schema(implementation = FoodItemDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida (item já existe ou dados incorretos).")
+    })
     public ResponseEntity<FoodItemDTO> criar(@RequestBody FoodItemDTO foodItemDTO) {
         FoodItemDTO salvo = foodItemService.salvar(foodItemDTO);
         return ResponseEntity.ok(salvo);
     }
 
-    // GET REQUEST- Listar todos os itens
+    // GET REQUEST - Listar todos os itens
     @GetMapping("/listar")
+    @Operation(summary = "Listagem de todos os itens da geladeira",
+            description = "Retorna uma lista de todos os itens cadastrados na geladeira.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lista de itens recuperada com sucesso.",
+                    content = @Content(schema = @Schema(implementation = FoodItemDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Nenhum item encontrado na geladeira.")
+    })
     public ResponseEntity<List<FoodItemDTO>> listar() {
         List<FoodItemDTO> foodItems = foodItemService.listar().stream()
                 .map(foodItemMapper::map)
@@ -40,6 +64,13 @@ public class FoodItemController {
 
     // GET REQUEST BY ID - Lista um item por ID
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Listagem de um item específico por ID",
+            description = "Retorna os detalhes de um item específico da geladeira pelo seu ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Item encontrado.",
+                    content = @Content(schema = @Schema(implementation = FoodItemDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Item não encontrado com o ID fornecido.")
+    })
     public ResponseEntity<?> listarPorId(@PathVariable Long id) {
         FoodItemDTO foodItemDTO = foodItemService.listarPorId(id);
         if (foodItemDTO != null) {
@@ -52,6 +83,12 @@ public class FoodItemController {
 
     // DELETE REQUEST - Remover item
     @DeleteMapping("/deletar/{id}")
+    @Operation(summary = "Deletar um item",
+            description = "Remove um item da geladeira pelo seu ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Item deletado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Item não encontrado com o ID fornecido.")
+    })
     public ResponseEntity<String> deletar(@PathVariable Long id) {
         FoodItemDTO foodItem = foodItemService.listarPorId(id);
         if (foodItem != null) {
@@ -63,8 +100,15 @@ public class FoodItemController {
         }
     }
 
-    // PATCH REQUEST- Atualizar parcialmente um item
+    // PATCH REQUEST - Atualizar parcialmente um item
     @PatchMapping("/atualizar/{id}")
+    @Operation(summary = "Atualizar parcialmente um item",
+            description = "Atualiza parcialmente os detalhes de um item da geladeira pelo seu ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Item atualizado com sucesso.",
+                    content = @Content(schema = @Schema(implementation = FoodItemDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Item não encontrado com o ID fornecido.")
+    })
     public ResponseEntity<?> atualizarParcialmente(@PathVariable Long id,
                                                    @RequestBody FoodItemDTO foodItemDTO) {
         FoodItemDTO atualizado = foodItemService.atualizarParcialmente(id, foodItemDTO);
--- src/main/java/dev/matheus/MagicFridgeAI/Controller/RecipeController.java
+++ src/main/java/dev/matheus/MagicFridgeAI/Controller/RecipeController.java
@@ -2,12 +2,17 @@ package dev.matheus.MagicFridgeAI.Controller;
 
 import dev.matheus.MagicFridgeAI.Service.ChatService;
 import dev.matheus.MagicFridgeAI.Service.FoodItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Recipes", description = "Endpoints para gerar receitas com base nos itens cadastrados na geladeira.")
 public class RecipeController {
 
     private final ChatService chatService;
@@ -19,6 +24,12 @@ public class RecipeController {
     }
 
     @GetMapping("/generate")
+    @Operation(summary = "Gera uma receita com base nos itens cadastrados na geladeira",
+            description = "Utiliza o gemini para sugerir uma receita criativa e saborosa com os ingredientes disponíveis na geladeira.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Mostra a receita gerada pelo gemini."),
+            @ApiResponse(responseCode = "400", description = "A IA não conseguiu gerar uma resposta para a sua solicitação!")
+    })
     private ResponseEntity<String> generateRecipeGemini() {
         String response = chatService.generateRecipeGemini(foodItemService.listar());
         if (response != null) {
